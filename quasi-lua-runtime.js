641202ea7d2d9819968dfd1d3ff6cb4a9df51d04
Uzair <abiduzair420@gmail.com>
2019-08-01 09:41:48 +0530

    runtime env created with load/store functionalities

---
+++ quasi-lua-runtime.js
@@ -0,0 +1,31 @@
+function Table() {
+  // Map from ES Harmony is a simple dictionary-style collection.
+  this.map = new Map;
+}
+
+Table.prototype = {
+  load: function (key) { return this.map.get(key); },
+  store: function (key, value) { this.map.set(key, value); }
+};
+
+function CHECK_TABLE(t) {
+  if (!(t instanceof Table)) {
+    throw new Error("table expected");
+  }
+}
+
+function LOAD(t, k) {
+  CHECK_TABLE(t);
+  return t.load(k);
+}
+
+function STORE(t, k, v) {
+  CHECK_TABLE(t);
+  t.store(k, v);
+}
+
+var os = new Table();
+
+STORE(os, 'clock', function () {
+  return Date.now() / 1000;
+});
